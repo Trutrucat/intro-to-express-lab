6f1d7b875b2b8e47178dc51f506f3304f1a1975f
Trutrucat <167733097+Trutrucat@users.noreply.github.com>
2024-07-02 09:08:41 -0700

    problem 3 start

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 const username = 'Mathilda'
 
 
-app.get('/greeting/username', (req, res) => {
+app.get('/greeting/:username', (req, res) => {
     res.send(`What a delight it is to see you once more, ${username}`)
 })
 
@@ -18,6 +18,17 @@ app.get('/roll/:number', (req, res) => {
 
         const roll = Math.floor(Math.random() * (maxNumber + 1))
         res.send(`You rolled a ${roll}.`)}
+})
+
+const collectibles = [
+    { name: 'shiny ball', price: 5.95 },
+    { name: 'autographed picture of a dog', price: 10 },
+    { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
+  ];
+app.get('/collectibles/:index', (req, res) => {
+    if(idx(0)){
+        res.send('So, you want the shiny ball? For 5.95 it can be yours!')
+    }
 })
 
 app.listen(3000, () => {
